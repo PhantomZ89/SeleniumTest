fa0efc02d4e177ff52d9d37dc2ed12ea6435d28a
PhantomZ89 <cshumtim@gmail.com>
2019-03-13 23:39:54 -0400

    Added more basic functionality

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using System.Threading.Tasks;
 
 namespace UnitTestProject1
 {
@@ -12,8 +13,10 @@ namespace UnitTestProject1
         public void TestMethod1()
         {
             IWebDriver driver = new ChromeDriver(@"D:\Projects\UnitTestProject1\UnitTestProject1");
+            driver.Manage().Window.Maximize();
 
-            driver.Navigate().GoToUrl("https://www,google.com");
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Task.Delay(5000).Wait();
 
             driver.Quit();
         }
